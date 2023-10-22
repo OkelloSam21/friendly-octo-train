07dfe3ceb42b8b8de78788ea2b2b15e1095443c3
Okello Sam <samuelokello929@gmail.com>
2023-10-22 14:57:29 +0300

    Common DataTypes

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -1,3 +1,5 @@
+import java.lang.Math.pow
+
 fun addInt(num1: Int, num2: Int): Int {
     val result = num1 + num2
     return result
@@ -6,10 +8,16 @@ fun addInt(num1: Int, num2: Int): Int {
 fun studentDetails(name: String, regNo: String, yearOfStudy: Int): String {
     return "name: $name \nregistration number: $regNo \nYear Of Study: $yearOfStudy \n"
 }
-fun binaryToDecimal(): Int{
-
+fun binaryToDecimal(): Double {
+    val leftMost = readln().toInt()
+    val left = readln().toInt()
+    val right = readln().toInt()
+    val rightMost = readln().toInt()
+    val result = leftMost * pow(2.0, 3.0)
+    return result
 }
 fun main() {
     addInt(4, 3)
-    print( studentDetails("Samuel", "COM/0053/21", 2))
+    print(studentDetails("Samuel", "COM/0053/21", 2))
+    binaryToDecimal()
 }
