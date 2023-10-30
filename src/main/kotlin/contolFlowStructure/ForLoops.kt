3a0c7c31c9b3c5f83387ed0184a58443a5bb1c3a
Okello Sam <samuelokello929@gmail.com>
2023-10-30 16:02:46 +0300

    for loops

--- src/main/kotlin/contolFlowStructure/ForLoops.kt
+++ src/main/kotlin/contolFlowStructure/ForLoops.kt
@@ -1,16 +1,15 @@
 package contolFlowStructure
 
 fun main() {
-
     /**
      * print values from 0 to 5 (5 is exclusive
      */
     for (i in 0 until 5)
 
-    /**
-     * print values from 0 t0 5
-     */
-    for (i in 0..5) println(i)
+        /**
+         * print values from 0 t0 5
+         */
+        for (i in 0..5) println(i)
     /**
      * print values of i from 10 to 0
      */
